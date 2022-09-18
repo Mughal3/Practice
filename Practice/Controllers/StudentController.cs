c507645d77447935ca984c2e76d7e70f232bb2f2
muzamil <muzamil@DESKTOP-UL2SOTR>
2022-09-18 16:30:57 +0500

    Add project files.

---
+++ Practice/Controllers/StudentController.cs
@@ -0,0 +1,62 @@
+﻿using Practice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentModel student = new StudentModel();
+            //ViewBag.First = "Muzamil";
+
+            student = SetStudentByModel();
+            SetStudentByViewBag();
+            SetStudentByViewData();
+            return View(student);
+        }
+        public StudentModel SetStudentByModel()
+        {
+            StudentModel student = new StudentModel()
+            {
+                StudentName ="Muzamil",
+                FatherName =" Sarwar",
+                Address =" Lahore",
+                Age=28
+                
+            };
+            return student;
+        }
+        public void SetStudentByViewBag()
+        {
+            StudentModel obj = new StudentModel()
+            {
+                StudentName = "Asad",
+                FatherName = " Azam",
+                Address = " ISB",
+                Age = 30
+            };
+
+            ViewBag.xyz=obj;
+        }
+
+        public void SetStudentByViewData()
+        {
+            StudentModel obj = new StudentModel()
+            {
+                StudentName = "Majid Ali",
+                FatherName = "Azam ",
+                Address = " ISB",
+                Age = 32
+            };
+
+            ViewData["vdata"] = obj;
+
+        }
+    }
+}
---
+++ Practice/Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice.Models
+{
+    public class StudentModel
+    {
+        public int Age { get; set; }
+        public string StudentName { get; set; }
+        public string FatherName { get; set; }
+        public string Address { get; set; }
+    }
+}
